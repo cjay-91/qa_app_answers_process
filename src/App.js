8d4ba3662dbca411925926e8b5a79c0b0b9cde90
cjay-91 <109164936+cjay-91@users.noreply.github.com>
2023-07-02 09:08:37 +0530

    add answer processing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {DataTable} from "primereact/datatable";
+import {Column} from "primereact/column";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css"; 
 
 function App() {
+
+  const data = [
+    {id:1, name: 'John', age: 20, city: 'New York'}, 
+    {id:2, name: 'Mary', age: 25, city: 'Paris'},
+    {id:3, name: 'Peter', age: 30, city: 'London'},
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="App">
+      <DataTable value={data} sortMode='multiple'>
+        <Column field="id" header="ID" sortable></Column>
+        <Column field="name" header="Name" sortable></Column>
+        <Column field="age" header="Age" sortable></Column>
+        <Column field="city" header="City" sortable></Column>
+      </DataTable>
     </div>
   );
 }
